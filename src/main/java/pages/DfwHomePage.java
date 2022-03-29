d869635f102b48b1fe0abf0be9e035d8e5f296b1
StevenIram1 <steven.Iram13@yahoo.com>
2022-03-28 22:38:19 -0500

    first commit

--- src/main/java/pages/DfwHomePage.java
+++ src/main/java/pages/DfwHomePage.java
@@ -6,44 +6,54 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class DfwHomePage  {
+public class DfwHomePage {
 	WebDriver driver;
-	
-	public DfwHomePage(WebDriver driver){
-		
+
+	public DfwHomePage(WebDriver driver) {
+
 		this.driver = driver;
-		}
+	}
 
-	
-	
-	
-	@FindBy(how = How.XPATH, using = "/html/body/reach-portal/div[3]/div/div/div/button")WebElement ACCEPT_BUTTON ;
-	@FindBy(how = How.XPATH, using = "//span[text() ='Park']")WebElement PARK_BUTTON ;
-	@FindBy(how = How.XPATH, using = "//*[contains(@id,'ad')]")WebElement ARRIVALDATE_BUTTON;
-	
+	@FindBy(how = How.XPATH, using = "/html/body/reach-portal/div[3]/div/div/div/button")
+	WebElement ACCEPT_BUTTON;
+	@FindBy(how = How.XPATH, using = "//span[text() ='Park']")
+	WebElement PARK_BUTTON;
+	@FindBy(how = How.XPATH, using = "//*[contains(@id,'ad')]")
+	WebElement ARRIVALDATE_BUTTON;
 
-	
-	public void acceptCookies() throws InterruptedException {
+	public void acceptCookies(){
 		ACCEPT_BUTTON.click();
-		Thread.sleep(3000);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	public void navigateToParkButton() throws InterruptedException {
+
+	public void navigateToParkButton(){
 		PARK_BUTTON.click();
-		Thread.sleep(3000);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
 	}
+
 	public String getPageTitle() {
 		return driver.getTitle();
 	}
-	
+
 	public void checkBottomInfo() {
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scroll(o,200");
 		js.executeScript("window.scroll(200,0");
 	}
-	
+
 	public void clickArrivallDateBox() {
 		ARRIVALDATE_BUTTON.click();
 	}
-	
-}
 
+}
